f1ba6224683d5e8a36435a016054f4d2f4b38789
Jimmy Einarsson <95jimein@skola.boras.se>
2023-02-16 10:56:13 +0100

    Add all my files

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ProtoTypePattern
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        List <string> options = new List<string>() {"Leather Seats", "Sunroof", "Navigation"};
+        Car c1 = new Car("Ford", "Mustang", 2019, options);
+        Car c2 = c1.Clone() as Car;
+        }
+    }
+    public abstract class Prototype
+    {
+        public abstract Prototype Clone();
+    }
+
+    public class Car : Prototype
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public List<string> Options { get; set; }
+
+        public Car(string make, string model, int year, List<string> options)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Options = options;
+        }
+        public override Prototype Clone()
+        {
+            return new Car(Make, Model, Year, Options);
+        }
+    }
+}
